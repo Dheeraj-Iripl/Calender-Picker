efa75739f38ea60d1fe42f6d2d15e6b794c5da69
DheVam <vanshidheeraj07@gmail.com>
2022-11-17 08:56:47 +0530

    Example code for calendar picker

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CalendarStrip from 'react-native-calendar-strip';
+
 
 export default function App() {
+  datesWhitelist = [
+    {
+      start: (Date),
+      end: (Date)
+    }
+  ]
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CalendarStrip
+        style={{ height: 120, paddingTop: 20, paddingBottom: 10 }}
+        calendarHeaderStyle={{ color: 'white', fontSize: 18, marginBottom: 20 }}
+        calendarColor={'#7743CE'}
+        dateNumberStyle={{ color: 'white', fontSize: 16 }}
+        dateNameStyle={{ color: 'white', fontSize: 16 }}
+        highlightDateNumberStyle={{ color: 'yellow' }}
+        highlightDateNameStyle={{ color: 'yellow' }}
+        iconLeft={require('./assets/left.png')}
+        iconRight={require('./assets/right.png')}
+        iconContainer={{ flex: 0.1 }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 70,
+    backgroundColor: "7743CE",
   },
 });
